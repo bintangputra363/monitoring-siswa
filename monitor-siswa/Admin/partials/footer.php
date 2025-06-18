8f687cc4d419d6dcec9e28cefea0066baa1af15e
bintangputra777 <bintangp2702@gmail.com>
2025-06-18 19:14:30 +0700

    Final

--- monitor-siswa/Admin/partials/footer.php
+++ monitor-siswa/Admin/partials/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Copyright. Bintang Putra Dwi Pamungkas
+                <script>document.write(new Date().getFullYear())</script> © Copyright. | Universitas Pamulang
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
